c1f3b21577fb78b5a895d3335fc7723a3db934fc
mohamed <mehdaoui200309@gmail.com>
2023-06-26 19:16:02 +0200

    fixing the animation

--- index.js
+++ index.js
@@ -21,16 +21,24 @@ export function show(i)
         radio+='<img src="'+users[i].img[j]+'" alt=""></img>'
     }
     let div = document.getElementById(i);
-    div.innerHTML='<div class="photo"><div class="imageshow"><img src="'+users[i].img[0]+'" alt=""></div><div class="radio">'+radio+'</div></div><div class="more_info"><p>'+users[i].titre+'</p><p>'+users[i].info+'</p><h2>$ '+users[i].price+'</h2><center><button onclick="add('+i+')">Add to Cart</button></center><center><h5 onclick="showless('+i+')">show less</h5></center></div>'
-    div.setAttribute('class','showpro');
-    div.setAttribute('onclick','');
-    let timeoutID = setTimeout(()=>{document.getElementById(i).scrollIntoView()}, 10);
+    let a = document.createElement("div")
+    a.innerHTML='<div class="photo"><div class="imageshow"><img src="'+users[i].img[0]+'" alt=""></div><div class="radio">'+radio+'</div></div><div class="more_info"><p>'+users[i].titre+'</p><p>'+users[i].info+'</p><h2>$ '+users[i].price+'</h2><center><button onclick="add('+i+')">Add to Cart</button></center><center><h5 onclick="showless('+i+')">show less</h5></center></div>'
+    a.setAttribute('class','showpro');
+    a.classList.add("fade-in");
+    div.parentNode.appendChild(a);
+    a.setAttribute('id',i+'a');
+    setTimeout(function() {
+        a.classList.remove("fade-in");
+        a.classList.add("fade-out");
+      }, 2);
+    // let timeoutID = setTimeout(()=>{document.getElementById(i).scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});}, 10);
 }
 export function showless(i)
 {
-    let div = document.getElementById(i);
-    div.innerHTML='<div class="image"><img src="'+users[i].img[0]+'" alt=""></div><div class="info"><p>'+users[i].titre+'</p><p>$'+users[i].price+'</p></div>';
-    div.setAttribute('class','prod');
-    div.setAttribute('id',i);
-    let timeoutID = setTimeout(()=>{div.setAttribute('onclick','show('+i+');');document.getElementById(i).scrollIntoView()}, 300);
+    let div = document.getElementById(i+'a');
+    div.parentNode.removeChild(div);
+    // div.innerHTML='<div class="image"><img src="'+users[i].img[0]+'" alt=""></div><div class="info"><p>'+users[i].titre+'</p><p>$'+users[i].price+'</p></div>';
+    // div.setAttribute('class','prod');
+    // div.setAttribute('id',i);
+    // let timeoutID = setTimeout(()=>{div.setAttribute('onclick','show('+i+');');document.getElementById(i).scrollIntoView()}, 3);
 }

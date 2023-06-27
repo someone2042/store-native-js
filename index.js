7298593c26594c9678c20ddcbf67c3e48646b43c
mohamed <mehdaoui200309@gmail.com>
2023-06-27 16:04:36 +0200

    adding effects

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import users from './list.json' assert{type: 'json'};
 
 const shop = document.getElementById("shop");
 let shoplist = new Array();
+let blur=false;
+if(blur)
+{
+    
+}
 
 for(let i=0;i<users.length;i++)
 {
@@ -30,17 +35,21 @@ export function show(i)
     a.innerHTML='<div class="photo"><div class="imageshow"><img src="'+users[i].img[0]+'" alt=""></div><div class="radio">'+radio+'</div></div><div class="more_info"><p>'+users[i].titre+'</p><p>'+users[i].info+'</p><h2>$ '+users[i].price+'</h2><center><button onclick="add('+i+')">Add to Cart</button></center><center><h5 onclick="showless('+i+')">show less</h5></center></div>'
     a.setAttribute('class','showpro');
     a.classList.add("fade-in");
-    div.parentNode.appendChild(a);
+    div.parentNode.parentNode.appendChild(a);
+    blur=true;
     a.setAttribute('id',i+'a');
     setTimeout(function() {
+        document.getElementById("shop").classList.add("face-out");
         a.classList.remove("fade-in");
         a.classList.add("fade-out");
-      }, 2);
+    }, 2);
 }
 export function showless(i)
 {
+    document.getElementById("shop").classList.remove("face-out");
     let div = document.getElementById(i+'a');
     div.parentNode.removeChild(div);
+    blur=false;
     let timeoutID = setTimeout(()=>{div.setAttribute('onclick','show('+i+');');document.getElementById(i).scrollIntoView()}, 3);
 }
 export function change(i,j)
